2f4c663ea8eadf49b0a5b113b78c6ab8fab47c50
JuanMa <mitild82@gmail.com>
2023-04-19 15:01:38 +0200

    Exercise 1
    - Created React app
    - Created a component, "Escena" and printed in the screen

---
+++ e-learn/src/App.js
@@ -0,0 +1,13 @@
+import Escena from "./components/escena/Escena";
+
+function App() {
+  return (
+    <div className="App">
+      <Escena
+        text={ "El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial" }
+      />
+    </div>
+  );
+}
+
+export default App;
---
+++ e-learn/src/components/escena/Escena.js
@@ -0,0 +1,9 @@
+const Escena = ( {text} ) => {
+  return(
+    <div>
+      <p>{ text }</p>
+    </div>
+  )
+}
+
+export default Escena
