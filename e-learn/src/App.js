1c5709bec6ea06a944fee36f038176ed4acddd82
JuanMa <mitild82@gmail.com>
2023-04-19 19:07:22 +0200

    Exercise 6
    - Changed the json file to a js file
    - Dynamically change background accordingly to the story the user is currently at

--- e-learn/src/App.js
+++ e-learn/src/App.js
@@ -1,15 +1,31 @@
 import { useState } from "react"
 import Escena from "./components/escena/Escena"
-import Textos from "./data.json"
+// import Textos from "./data.json"
+import { stories } from "./data"
 import Button from "./components/escena/Button"
 import Portada from "./components/escena/Portada"
+import styled from "styled-components"
 
 function App() {
   const [ current, setCurrent ] = useState(0)
   const [ isStarted, setIsStarted ] = useState(false)
-  const textos = Textos.textos
-  const escenas = textos.map((texto, index) => <Escena key={index} text={texto} bg={ current === index ? 'pink' : 'white' } />
+  // const textos = Textos.textos
+  // const escenas = textos.map((texto, index) => <Escena key={index} text={texto} bg={ current === index ? 'pink' : 'white' } />
+  // )
+
+  const escenas = stories.map((story, index) => <Escena key={index} text={story.txt} bg={ current === index ? 'pink' : 'white' } />
   )
+
+  const bgImage = isStarted ? stories[current].img : ""
+
+    const StyledBg = styled.div`
+      background-image: url('${bgImage}');
+      background-color: white;
+      background-repeat: no-repeat;
+      background-size: cover;
+      background-position: center;
+      height: 100vh;
+    `;
 
   function handleStart() {
     setIsStarted(true)
@@ -32,7 +48,7 @@ function App() {
   }
 
   return (
-    <div className="App">
+    <StyledBg>
       {
         !isStarted ? 
         <>
@@ -44,9 +60,8 @@ function App() {
         <Button handleClick={ handleNext } text={ 'Següent' } />
         { escenas }
         </>
-      }
-      
-    </div>
+      }      
+    </StyledBg>
   );
 }
 
---
+++ e-learn/src/data.js
@@ -0,0 +1,18 @@
+export const stories = [
+  {
+    txt: "El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial",
+    img: "/img/1.jpg"
+  },
+  {
+    txt: "Sentia curiositat per l'interior de la nau i es va posar a inspeccionar-la. Va arribar a una sala amb dues portes.",
+    img: "/img/2.jpg"
+  },
+  {
+    txt: "L'heroi va decidir travessar la porta que el portava a casa",
+    img: "/img/3.jpg"
+  },
+  {
+    txt: "Mentrestant, altres herois no van tenir tanta sort en la seva elecció …",
+    img: "/img/4.jpg"
+  },
+]
